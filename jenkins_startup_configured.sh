db2865a415559f35fca723d285bf3605fb6360ed
Jared A <github@myprolixus.com>
2018-11-24 16:54:28 -0700

    Update jenkins_startup_configured.sh
    Assume 'hostname' returns fully qualified responses

--- jenkins_startup_configured.sh
+++ jenkins_startup_configured.sh
@@ -8,7 +8,7 @@ echo "Jenkins should now be running"
 echo
 echo "Linux Academy Playground servers are in the mylabserver.com domain"
 echo "In your browser now visit the Jenkins startup on port 8080"
-echo "For example http://$(hostname).mylabserver.com:8080"
+echo "For example http://$(hostname):8080"
 echo
 echo "See initial adminPASSWORD on next line:"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword

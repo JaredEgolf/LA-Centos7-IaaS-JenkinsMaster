5d7de293f1ee953fe3206248bba4f31b3df6c9ab
Jared A <github@myprolixus.com>
2018-11-24 16:53:30 -0700

    Update jenkins_startup_configured.sh
    Improved echo parsing.

--- jenkins_startup_configured.sh
+++ jenkins_startup_configured.sh
@@ -4,9 +4,12 @@ sudo systemctl enable jenkins
 sudo systemctl start jenkins
 systemctl status jenkins
 
+echo "Jenkins should now be running"
+echo
 echo "Linux Academy Playground servers are in the mylabserver.com domain"
 echo "In your browser now visit the Jenkins startup on port 8080"
 echo "For example http://$(hostname).mylabserver.com:8080"
+echo
 echo "See initial adminPASSWORD on next line:"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 

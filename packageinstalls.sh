8188a5fe204ab71816510c09d166c6acf1b71d9b
Jared A <github@myprolixus.com>
2018-11-24 16:39:51 -0700

    Update packageinstalls.sh
    Add additional checks to confirm command requirements.

--- packageinstalls.sh
+++ packageinstalls.sh
@@ -3,8 +3,8 @@ which java || sudo yum install java-1.8.0-openjdk-devel -y
 
 which wget || sudo yum install wget -y
 
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+ls /etc/yum.repos.d/jenkins.repo || sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-sudo yum install jenkins -y
+rpm -qa jenkins || sudo yum install jenkins -y
 
 which git || sudo yum install git -y

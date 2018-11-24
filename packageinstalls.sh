f1020d4e3b30b13617093e8fc4284039f51f066a
Jared A <github@myprolixus.com>
2018-11-24 16:04:10 -0700

    Create packageinstalls.sh
    Steps to install prereqs and jenkins

---
+++ packageinstalls.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+which java || sudo yum install java-1.8.0-openjdk-devel -y
+
+which wget || sudo yum install wget -y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repos
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum install jenkins -y
+
+which git || sudo yum install git -y

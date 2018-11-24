f0599da5e95d6b79c0ef3566cfd270122280905f
Jared A <github@myprolixus.com>
2018-11-24 16:52:24 -0700

    Update jenkins_startup_configured.sh
    Adding next steps for browser connection requirements

--- jenkins_startup_configured.sh
+++ jenkins_startup_configured.sh
@@ -4,4 +4,9 @@ sudo systemctl enable jenkins
 sudo systemctl start jenkins
 systemctl status jenkins
 
+echo "Linux Academy Playground servers are in the mylabserver.com domain"
+echo "In your browser now visit the Jenkins startup on port 8080"
+echo "For example http://$(hostname).mylabserver.com:8080"
+echo "See initial adminPASSWORD on next line:"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+

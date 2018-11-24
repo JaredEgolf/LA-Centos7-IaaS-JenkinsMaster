cb1a07cc104388176e1221a5a72781e8242426dd
Jared A <github@myprolixus.com>
2018-11-24 16:35:02 -0700

    Update packageinstalls.sh
    corrected typo on jenkins repo

--- packageinstalls.sh
+++ packageinstalls.sh
@@ -3,7 +3,7 @@ which java || sudo yum install java-1.8.0-openjdk-devel -y
 
 which wget || sudo yum install wget -y
 
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repos
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 sudo yum install jenkins -y
 

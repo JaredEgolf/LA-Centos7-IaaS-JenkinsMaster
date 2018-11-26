54935c3cc44a80f32be329a7aed965a1789f7bf6
Jared A <github@myprolixus.com>
2018-11-25 17:00:48 -0700

    Update git-jenkins-init.sh
    Added an initial commit.  .gitignore needs more work
    Also need a better handle on finding $JENKINS_HOME when not set in the environment

--- git-jenkins-init.sh
+++ git-jenkins-init.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 # Initialize and make a first git commit to begin versioning $JENKINS_HOME
 echo $JENKINS_HOME && cd $JENKINS_HOME
+pwd && cd /var/lib/jenkins
 ls -alrt
 git init
+git config --global user.name LA Student
+git config --global user.email lastudent@myprolixus.com
+echo "**/logs" >> .gitignore
+echo "workspace" >> .gitignore
+echo "secrets" >> .gitignore
+echo "jobs/" >> .gitignore
+echo "secret*" >> .gitignore
+git add .
+git commit -a -m "Initial Commit"

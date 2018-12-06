b4df5c520b63aaea3b19381a1a2b1da709290147
Jared A <github@myprolixus.com>
2018-12-06 08:01:39 -0700

    Update packageinstalls.sh

--- packageinstalls.sh
+++ packageinstalls.sh
@@ -5,6 +5,6 @@ which wget || sudo yum install wget -y
 
 ls /etc/yum.repos.d/jenkins.repo || sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-rpm -qa jenkins || sudo yum install jenkins -y
+sudo yum install jenkins -y
 
 which git || sudo yum install git -y
